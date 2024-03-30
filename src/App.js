0a63d49c1d33a6214e5bccd8669f70b6c5d11766
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-03-30 15:10:58 +0530

    Dispatching an action to increment in a crude way

--- src/App.js
+++ src/App.js
@@ -1,3 +1,82 @@
+import React, { useReducer, useEffect } from "react";
+import Button from "./components/Button";
+
+const reducer = (state, action) => {
+  return {
+    ...state,
+    count: state.count + 1,
+  };
+};
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    valueToAdd: 0,
+  });
+
+  function increment() {
+    dispatch(); // Dispatching an action which reaches the reducer
+  }
+
+  function decrement() {}
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!state.valueToAdd) return;
+  }
+
+  function handleAddValue() {}
+
+  useEffect(function () {
+    document.title = "Counter useReducer";
+  }, []);
+
+  return (
+    <div className="app">
+      <Counter
+        count={state.count}
+        increment={increment}
+        decrement={decrement}
+      />
+
+      <AddForm
+        valueToAdd={state.valueToAdd}
+        setValueToAdd={handleAddValue}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
+}
+
+function Counter({ count, increment, decrement }) {
+  return (
+    <div className="counter">
+      <h2>Current Count: {count}</h2>
+
+      <Button onClick={decrement}>decrement</Button>
+      <Button onClick={increment}>increment</Button>
+    </div>
+  );
+}
+
+function AddForm({ valueToAdd, setValueToAdd, handleSubmit }) {
+  return (
+    <>
+      <h2>Add a lot:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={valueToAdd || ""}
+          onChange={(e) => setValueToAdd(parseInt(e.target.value))}
+        />
+
+        <Button type="submit">Add</Button>
+      </form>
+    </>
+  );
+}
+
+/** BELOW CODE IS WITHOUT useReducer ---
 import React, { useState, useEffect } from "react";
 import Button from "./components/Button";
 
@@ -68,3 +147,4 @@ function AddForm({ valueToAdd, setValueToAdd, handleSubmit }) {
     </>
   );
 }
+-- */
